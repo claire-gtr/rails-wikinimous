488b8e91a369ae9d07a6cab6ba706b4cdb43dd77
Claire Gautier <claire.gautier@edhec.com>
2018-05-08 17:43:05 +0200

    ex2

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
+require 'faker'
+
+10.times do
+  new_article = Article.new(title: Faker::Book.title, content: Faker::Book.genre)
+  new_article.save
+end
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
